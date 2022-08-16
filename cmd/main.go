e7b64e5844e9c4e1793e3ca99baa3650f7eb6940
Abutalif <abutaliph@gmail.com>
2022-08-16 21:46:14 +0600

    added net.Conn and bufio.Reader

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,87 @@
 package main
 
+import (
+	"bufio"
+	"net"
+	"os"
+)
+
 func main() {
+	isServer, err := CheckMode()
+	if err != nil {
+		// TODO: log error
+		return
+	}
+	if isServer {
+		err = ServerMode()
+	} else {
+		err = ClientMode()
+	}
+	if err != nil {
+		// TODO: log error
+		return
+	}
+}
+
+// Returns True for Server mode, False for Client mode
+func CheckMode() (bool, error) {
+	// TODO:
+	// hanlde flags
+	//-l listen
+	// also
+	args := os.Args
+	if len(args) == 0 {
+		// TODO: log error
+		return false, nil // TODO: error should not be nil
+	}
+
+	return true, nil
+}
+
+// Runs a Server
+func ServerMode() error {
+	lstn, err := net.Listen("tcp", "localhost:8080") // TODO: default or changable host
+	// errChan := make(chan error)
+	// msgChan := make(chan string)
+	if err != nil {
+		// TODO: log error
+		return err
+	}
+	defer lstn.Close()
+
+	// TODO:
+	// connMap := &sync.Map{}
+	users := make(map[int]net.Conn)
+	i := 0
+	for {
+		conn, err := lstn.Accept()
+		if err != nil {
+			break
+		}
+		users[i] = conn
+		i++
+
+		go HandleNewUser(conn, user)
+	}
+	// should return nil if correct, else err
+	return err
+}
+
+func HandleNewUser(conn net.Conn) {
+	defer conn.Close()
+	for {
+		userInput, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		for 
+	}
+}
+
+// Runs a Client
+func ClientMode() error {
+	// TODO: connect to server with a given IP
+	// TODO: create a file with a penguin
+	return nil
 }
